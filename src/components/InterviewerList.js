43914797cc16f3d2f9e6e858dc1f34265e751771
MattLawson98 <matthewlawson50@gmail.com>
2022-04-28 17:03:52 -0700

    This file helps create a list of interviewers!

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,31 @@
+
+import React from "react";
+import InterviewerListItem from "./InterviewListItem";
+import "components/InterviewerList.scss";
+import PropTypes from 'prop-types';
+
+
+export default function interviewerList(props) {
+  
+  const interviewers = props.interviewers.map(interviewer => {
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.value}
+        setInterviewer={event => props.onChange(interviewer.id)}
+      />
+    );
+  });
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{interviewers}</ul>
+    </section>
+  )
+}
+
+interviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
